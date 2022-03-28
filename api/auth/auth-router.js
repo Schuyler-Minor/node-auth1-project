02162cd1b5d276aaddd05823c008a208b6f35808
Schuyler <skym1n0r91@gmail.com>
2022-03-28 18:19:50 -0500

    login succesful

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -63,7 +63,13 @@ router.post(
   }
  */
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  res.json("login");
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user;
+    res.status(200).json({ message: `Welcome ${req.user.username}` });
+  } else {
+    next({ status: 401, message: "Invalid credentials" });
+  }
 });
 
 /**
